ee6aa88f2c0024c59d62ce8331c06242c36021ec
xaviescu <xavier.escudero@gmail.com>
2026-09-01 09:34:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // - New SW activates immediately on install (skipWaiting) and forces open tabs
 //   to reload so the new version takes effect without "clear cache" dance.
 
-const CACHE = 'eos-xe-v116';
+const CACHE = 'eos-xe-v117';
 const SHELL = [
   './',
   './index.html',
